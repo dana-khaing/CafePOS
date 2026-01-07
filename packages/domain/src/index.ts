7d5236f643c20432c1a1abf3afb1407cd1db0cb5
Dana Khaing <dana1352000.uk@gmail.com>
2026-01-07 09:00:00 +0000

    feat: define versioned sync event contracts

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -2,5 +2,6 @@ export const PRODUCT_NAME = 'CafePOS'
 
 export * from './access.js'
 export * from './money.js'
+export * from './sync-event.js'
 export * from './tax.js'
 export * from './totals.js'
