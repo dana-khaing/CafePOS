7bd1b5f8bd140d80fcae3ab7211835985806fc2c
Dana Khaing <dana1352000.uk@gmail.com>
2026-01-07 09:15:00 +0000

    feat: implement retryable sync outbox

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -2,6 +2,7 @@ export const PRODUCT_NAME = 'CafePOS'
 
 export * from './access.js'
 export * from './money.js'
+export * from './outbox.js'
 export * from './sync-event.js'
 export * from './tax.js'
 export * from './totals.js'
